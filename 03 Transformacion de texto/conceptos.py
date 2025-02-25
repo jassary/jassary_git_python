10334e880bf6f6765c6475875e5ce564186aa535
jassary <jassaryespinoza@gmail.com>
2025-02-24 22:27:41 -0600

    clases de python

--- 03 Transformacion de texto/conceptos.py
+++
@@ -1,4 +0,0 @@
-#str.upper() → Convierte a mayúsculas.
-#str.lower() → Convierte a minúsculas.
-#str.title() → Primera letra en mayúscula de cada palabra.
-#str.capitalize() → Solo la primera letra en mayúscula.
---
+++ 03-transformadores_de_texto/conceptos.py
@@ -0,0 +1,29 @@
+#str.upper() → Convierte a mayúsculas.
+#str.lower() → Convierte a minúsculas.
+#str.title() → Primera letra en mayúscula de cada palabra.
+#str.capitalize() → Solo la primera letra en mayúscula
+#str.replace("old", "new") → Reemplaza caracteres o palabras.
+#str.lstrip() → Elimina espacios a la izquierda.
+#str.rstrip() → Elimina espacios a la derecha.
+#str.strip() → Elimina espacios en blanco al inicio y al final
+
+
+#10. str.startswith(substring) – Verifica si una cadena comienza con un valor específico.
+#str.endswith(substring) – Verifica si una cadena termina con un valor específico.
+#str.find(substring) – Retorna la primera posición donde se encuentra el substring, o -1 si no
+#lo encuentra.
+#str.index(substring) – Igual que find(), pero lanza un error si no encuentra la subcadena.
+#str.count(substring) – Cuenta cuántas veces aparece un substring.
+#División y unión de cadenas
+#str.split(sep) – Divide una cadena en una lista usando un separador.
+#str.rsplit(sep, maxsplit) – Divide desde la derecha, útil cuando se quiere limitar la cantidad
+#de cortes.
+#str.partition(sep) – Divide en tres partes: antes, el separador y después.
+#str.rpartition(sep) – Igual que partition(), pero desde la derecha.
+#' '.join(iterable) – Une una lista de cadenas en una sola con el separador indicado.
+#Verificación de contenido
+#str.isdigit() – Verifica si todos los caracteres son dígitos.
+#str.isalpha() – Verifica si todos los caracteres son letras.
+#str.isalnum() – Verifica si la cadena es alfanumérica (letras y números).
+#str.isspace() – Verifica si la cadena solo contiene espacios.
+#str.islower() – Verifica si la cadena está en minúsculas.
